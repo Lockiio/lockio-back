c970cb21f59be2fcc9f57cd569c665980dee76e2
Darhan <adam.khalepo@protonmail.com>
2023-01-27 08:19:03 +0100

    add: first commit

---
+++ src/main/java/com/miage/lockio/lockioback/LockioBackApplication.java
@@ -0,0 +1,13 @@
+package com.miage.lockio.lockioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockioBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockioBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miage/lockio/lockioback/LockioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.lockio.lockioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockioBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
