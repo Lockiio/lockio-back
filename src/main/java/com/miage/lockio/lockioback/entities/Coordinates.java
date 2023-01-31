3cb806501ca7d7deafdf938d49ff15117ff222bd
AdamKhalepo <adam.khalepo@protonmail.com>
2023-01-31 16:48:40 +0100

    fix: add class Coordinates

---
+++ src/main/java/com/miage/lockio/lockioback/entities/Coordinates.java
@@ -0,0 +1,15 @@
+package com.miage.lockio.lockioback.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coordinates {
+    public double latitude;
+    public double longitude;
+}
