ff839e0e22c1d1609c761c09f93662335879cbb0
RémiSaurel <r.saurel5@gmail.com>
2023-03-12 17:33:33 +0100

    add: prereserved function

--- src/main/java/com/miage/lockio/lockioback/api/ApiLockio.java
+++ src/main/java/com/miage/lockio/lockioback/api/ApiLockio.java
@@ -9,11 +9,13 @@ import lombok.Setter;
 @Setter
 public class ApiLockio {
     private Long id;
+    private Long blockId;
     private Long localId;
     private LockioSize size;
     private LockioStatus status;
     public ApiLockio(Lockio lockio) {
         this.id = lockio.getId();
+        this.blockId = lockio.getBlock().getId();
         this.localId = lockio.getLocalId();
         this.size = lockio.getSize();
         this.status = lockio.getStatus();
